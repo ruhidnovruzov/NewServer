da1e066fb991cc01579c297fcb19aa89b69fb053
ruhidnovruzovv <ruhidnovruzov2@gmail.com>
2025-04-26 09:19:44 +0400

    testt

--- services/schedulerService.js
+++ services/schedulerService.js
@@ -7,7 +7,7 @@ const { determineWeekType, getDayName, getTomorrowInfo } = require('../utils/sch
 // Bütün bildiriş planlaşdırıcılarını başlatmaq
 const initSchedulers = () => {
   // Hər gün axşam 20:00-da sabahkı dərs cədvəli haqqında bildiriş
-  cron.schedule('15 09 * * *', async () => {
+  cron.schedule('23 09 * * *', async () => {
     try {
       console.log('Running evening notification job...');
       
